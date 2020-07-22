f6d43d15207d84bfa2c200e98eb6bd4c0906d0a4
Yashpal <yashpal@ericsson.com>
2020-07-22 22:38:14 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './SideBar/sidebar/sidebar.component';
+import { DashboardComponent } from './DashBoard/dashboard/dashboard.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { MatSidenavModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
